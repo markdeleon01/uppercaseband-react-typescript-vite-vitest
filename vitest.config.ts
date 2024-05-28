921ffcb843784e6dd36021dc099d63e0159f2a9c
Mark de Leon <mark_deleon@hotmail.com>
2024-05-28 13:28:34 +0800

    Istanbul code coverage and badges

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,17 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false
+    })
+  ],
   server: {
     host: true,
     port: 5173,
@@ -15,5 +22,26 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+      ],
+      reporter: ['json-summary', 'html'],
+      provider: 'istanbul'
+    }
   },
 });
