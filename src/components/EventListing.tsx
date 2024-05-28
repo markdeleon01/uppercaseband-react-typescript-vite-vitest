ca3d8b5e1ed2dc768be4744cba695475d2fc80cb
Mark de Leon <mark_deleon@hotmail.com>
2024-05-28 16:11:38 +0800

    Updated markup and styling tweaks

--- src/components/EventListing.tsx
+++ src/components/EventListing.tsx
@@ -1,6 +1,6 @@
 import './EventListing.css'
 import { formatDate } from '../utils/dateFormatter'
-import ReactHtmlParser from 'react-html-parser'
+import parse from 'html-react-parser'
 import { Event } from '../store/events'
 
 function EventListing(props: { event: Event }) {
@@ -14,7 +14,7 @@ function EventListing(props: { event: Event }) {
 			<br />
 			<span data-testid='event-location' className='event-location'>{event.location}</span>
 			<br />
-			<span data-testid='event-content' className='event-content'>{ReactHtmlParser(event.content)}</span>
+			<span data-testid='event-content' className='event-content'>{parse(event.content)}</span>
 			<a
                 data-testid='event-url' 
 				className='event-url'
